08c696f945b487dde875f4373501cd408b4a899c
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-14 23:11:09 -0300

    Update Gerenciar Reservas.py

--- Gerenciar Reservas.py
+++ Gerenciar Reservas.py
@@ -5,19 +5,28 @@ from services.carregar_dados import carregar_dataframe
 from controller.reservas_controller import controlar_nova_reserva
 
 st.set_page_config(layout="wide")
-
 st.title("Sistema de Gerenciamento de Reservas Fuji")
 st.markdown("### Organize, registre e acompanhe suas reservas de forma prática e segura em tempo real.")
 
-#Cria conexão
 if "aba" not in st.session_state:
     st.session_state.aba = conectar_planilha()
+
+# Guarda o dataframe no estado para evitar recarregar sempre
+if "df_reservas" not in st.session_state:
+    st.session_state.df_reservas = carregar_dataframe()
+
 aba = st.session_state.aba
 
 with st.form("form_reserva"):
     dict_dados = obter_dados_reserva()
     enviado = st.form_submit_button("Adicionar Reserva")
     if enviado:
-        df_reservas=carregar_dataframe()
-        controlar_nova_reserva(df_reservas,dict_dados["Data"],dict_dados,aba)
+        controlar_nova_reserva(st.session_state.df_reservas, dict_dados["Data"], dict_dados, aba)
+        
+        # Recarrega o dataframe atualizado após gravar na planilha
+        st.session_state.df_reservas = carregar_dataframe()
+        
         st.success("Reserva adicionada com sucesso!")
+
+# Exibe o dataframe atualizado na tela
+st.dataframe(st.session_state.df_reservas)
