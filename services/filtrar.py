7f3adfeff579fe1d0ebba271f686291d512097ef
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-14 22:40:04 -0300

    Update filtrar.py

--- services/filtrar.py
+++ services/filtrar.py
@@ -1,2 +1,5 @@
-def filtrar_dataframe(df_reservas,data):
-    return df_reservas.loc[df_reservas["Data"]==data]
+import pandas as pd
+def filtrar_dataframe(df_reservas, data):
+    if isinstance(data, str):
+        data = pd.to_datetime(data, format="%d/%m/%Y", errors="coerce").date()
+    return df_reservas.loc[df_reservas["Data"] == data]
