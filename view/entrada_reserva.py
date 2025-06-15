6129f4f65c2dc36ebdf92a57379f88316437569a
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-14 21:14:34 -0300

    Update entrada_reserva.py

--- view/entrada_reserva.py
+++ view/entrada_reserva.py
@@ -5,6 +5,7 @@ def obter_dados_reserva():
     col1,col2=st.columns(2)
     with col1:
         data=st.date_input("Data da Reserva",value=datetime.today(),format="DD/MM/YYYY")
+        data_formatada=data.strftime("%d/%m/%Y")
         nome=st.text_input("Nome do Cliente")
         quantidade_pessoas=st.number_input("Número de Pessoas",min_value=0)
     with col2:
@@ -13,7 +14,7 @@ def obter_dados_reserva():
         area = st.radio("Área do Restaurante", ["Interno", "Externo"],horizontal=True)
     observacoes=st.text_input("Observações")
     return {
-        "Data": data,
+        "Data": data_formatada,
         "Horário": horario,
         "Nome": nome,
         "Telefone": telefone,
