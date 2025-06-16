625c448584c34712b333600d08da0ad9ebba715b
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-15 23:24:03 -0300

    s

--- services/filtrar.py
+++ services/filtrar.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import streamlit as st
 def filtrar_dataframe(df_reservas, data):
     if isinstance(data, str):
         data = pd.to_datetime(data, format="%d/%m/%Y", errors="coerce").date()
-    oi=df_reservas.loc[df_reservas["Data"] == data]
-    st.write(oi)
-    return oi
+    return df_reservas.loc[df_reservas["Data"].dt.date  == data]
