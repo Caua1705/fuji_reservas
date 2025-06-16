aa3c7d409d9713db067f9dadca566968090a215e
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-16 13:48:19 -0300

    ju

--- pages/Gerenciar Reservas.py
+++ pages/Gerenciar Reservas.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from services.carregar_dados import carregar_dataframe
+from utils.formatadores import formatar_dados
 
 if "df_reservas" not in st.session_state:
     st.session_state.df_reservas = carregar_dataframe()
 
 df_reservas=st.session_state.df_reservas
+
+df=formatar_dados(df_reservas)
+
+
 
 
 st.write(df_reservas)
