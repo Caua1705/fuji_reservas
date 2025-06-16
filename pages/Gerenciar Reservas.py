67f7789bf8dbf6d07bbd4dde880c3b907d600d6a
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-16 14:20:26 -0300

    M

--- pages/Gerenciar Reservas.py
+++ pages/Gerenciar Reservas.py
@@ -14,7 +14,7 @@ df_reservas = st.session_state.df_reservas
 df_reservas["Data"] = pd.to_datetime(df_reservas["Data"], errors="coerce").dt.date
 
 st.title("📅 Gerenciamento de Reservas")
-data_selecionada = st.date_input("Selecione o dia para gerenciar as reservas", datetime.today().date(),format="%d,%m,%Y")
+data_selecionada = st.date_input("Selecione o dia para gerenciar as reservas", datetime.today().date(),format="DD/MM/YYYY")
 
 # Filtrar reservas
 reservas_dia = filtrar_df_reservas(df_reservas,data_selecionada)
