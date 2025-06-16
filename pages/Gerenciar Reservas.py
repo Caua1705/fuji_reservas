5ba16b384bf1b1aa220d5ea2cafa008c935d8a42
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-16 13:32:04 -0300

    adicionado pagina

---
+++ pages/Gerenciar Reservas.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from services.carregar_dados import carregar_dataframe
+
+if "df_reservas" not in st.session_state:
+    st.session_state.df_reservas = carregar_dataframe()
+
+df_reservas=st.session_state.df_reservas
+
+st.write(df_reservas)
