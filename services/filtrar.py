9e65ad4652278e962bca850b7d5e12f3349d5d94
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-15 23:19:57 -0300

    s

--- services/filtrar.py
+++ services/filtrar.py
@@ -2,4 +2,4 @@ import pandas as pd
 def filtrar_dataframe(df_reservas, data):
     if isinstance(data, str):
         data = pd.to_datetime(data, format="%d/%m/%Y", errors="coerce").date()
-    return df_reservas.loc[df_reservas["Data"] == data]
+    return df_reservas.loc[df_reservas["Data"] == data.date()]
