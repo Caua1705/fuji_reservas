d885824c87618e63831daffd43e73398133a588a
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-16 13:51:28 -0300

    s

--- pages/Gerenciar Reservas.py
+++ pages/Gerenciar Reservas.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from services.carregar_dados import carregar_dataframe
-from utils.formatadores import formatar_dados
+import pandas as pd
 
 if "df_reservas" not in st.session_state:
     st.session_state.df_reservas = carregar_dataframe()
 
 df_reservas=st.session_state.df_reservas
 
-df=formatar_dados(df_reservas)
-
-
+df_reservas["Data"]=pd.to_datetime(df_reservas["Data"],errors="coerce")
 
 
 st.write(df_reservas)
