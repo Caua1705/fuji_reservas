7195dad7c95b14b498b394aa41ef48ae20e0c27f
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-17 14:48:12 -0300

    jj

--- model/adicionar.py
+++ model/adicionar.py
@@ -2,8 +2,14 @@
 def registrar_reserva(nova_linha,aba):
     aba.append_row(nova_linha)
 
-def registrar_reservas_por_dia(linhas,filial,aba2,aba3):
-    if filial=="Aldeota":
+def registrar_reservas_por_dia(linhas, filial, aba2, aba3):
+    if filial == "Aldeota":
+        cabecalho = aba2.get_all_values()[0]  # pega primeira linha (cabeçalho)
+        aba2.clear()
+        aba2.append_row(cabecalho)
         aba2.append_rows(linhas)
-    elif filial=="Cambeba":
+    elif filial == "Cambeba":
+        cabecalho = aba3.get_all_values()[0]
+        aba3.clear()
+        aba3.append_row(cabecalho)
         aba3.append_rows(linhas)
