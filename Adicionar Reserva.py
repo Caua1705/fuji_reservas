5767711693b2f1357db14c4e38986f5d43831c71
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-17 14:08:19 -0300

    k

--- Adicionar Reserva.py
+++ Adicionar Reserva.py
@@ -39,6 +39,6 @@ with st.form("form_reserva", border=True):
                 aba
             )
             st.session_state.df_reservas = carregar_todas_as_reservas()
-            controlar_reservas_por_dia(st.session_state.df_reservas,dict_dados["Unidade"])
+            controlar_reservas_por_dia(st.session_state.df_reservas,dict_dados["Unidade"],aba2,aba3)
             st.success("✅ Reserva adicionada com sucesso!")
 
