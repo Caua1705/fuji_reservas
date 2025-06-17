219db5088c9e5b864ffe1f554738d286581d4fce
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-17 14:42:36 -0300

    k

--- services/agrupar.py
+++ services/agrupar.py
@@ -1,2 +1,5 @@
+import pandas as pd
+
 def agrupar_por_dia(df):
+    df["Número de Pessoas"]=pd.to_numeric(df["Número de Pessoas"])
     return df.groupby("Data")["Número de Pessoas"].sum().reset_index()
