cfa7c1c6014a4368fb3b37927a165d88020eb28f
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-17 14:26:49 -0300

    j

--- services/agrupar.py
+++ services/agrupar.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def agrupar_por_dia(df):
-    df_agrupado=df.groupby("Data")["Número de Pessoas"],sum()
+    df_agrupado=df.groupby("Data")["Número de Pessoas"].sum()
     st.write(df_agrupado)
