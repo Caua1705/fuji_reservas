04fb2cb8b556a936603d15e47679761c4b32916a
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-17 10:11:47 -0300

    s

--- utils/formatadores.py
+++ utils/formatadores.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-ORDEM_CAMPOS_RESERVA = ["Data", "Horário", "Nome", "Telefone", "Email," "Número de Pessoas", "Filial", "Área do Restaurante", "Observações"]
+ORDEM_CAMPOS_RESERVA = ["Data", "Horário", "Nome", "Telefone", "Email", "Número de Pessoas", "Filial", "Área do Restaurante", "Observações"]
 
 def formatar_nova_linha(dict_dados,ordem_campos):
      return [str(dict_dados[campo]) for campo in ordem_campos]
