af5d3757454813f22c308e432230a13342d41aeb
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-17 15:05:52 -0300

    z

--- services/agrupar.py
+++ services/agrupar.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 def agrupar_por_dia(df):
-    df["Número de Pessoas"]=pd.to_numeric(df["Número de Pessoas"])
-    return df.groupby("Data")["Número de Pessoas"].sum().reset_index()
+    df["Número de Pessoas"] = pd.to_numeric(df["Número de Pessoas"])
+    df_agrupado = df.groupby("Data")["Número de Pessoas"].sum().reset_index()
+    df_agrupado["Data"] = df_agrupado["Data"].dt.strftime("%d/%m/%Y")
+    return df_agrupado
