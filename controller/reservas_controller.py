91a24146bfd1f48dafadb2ed3f2478a93b4b6f41
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-18 14:41:56 -0300

    W

--- controller/reservas_controller.py
+++ controller/reservas_controller.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from services.reservas import processar_nova_reserva,processar_reservas_agrupadas
+from services.reservas import processar_nova_reserva
 from services.email import enviar_email_cliente
 
 def controlar_nova_reserva(df_reservas, data, dict_dados, aba):
