a12adef105a3a2b399a99031389f03b73254ff93
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-18 10:26:16 -0300

    d

--- services/agrupar.py
+++ services/agrupar.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 def agrupar_por_dia(df):
-    df_agrupado = df.groupby("Data")["Número de Pessoas"].sum().reset_index()
+    # df_agrupado = df.groupby("Data")["Número de Pessoas"].sum().reset_index()
     # df_agrupado["Data"] = pd.to_datetime(df_agrupado["Data"], errors="coerce")
     # df_agrupado["Data"] = df_agrupado["Data"].dt.date
-    return df_agrupado
+    return df
