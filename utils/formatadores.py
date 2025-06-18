2eaac0a750736c5071400a2b9722ac969a5483bd
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-18 15:18:04 -0300

    j

--- utils/formatadores.py
+++ utils/formatadores.py
@@ -6,7 +6,7 @@ def formatar_nova_linha(dict_dados,ordem_campos):
      return [str(dict_dados[campo]) for campo in ordem_campos]
 
 def formatar_dados(df):
-     df["Data"]=pd.to_datetime(df["Data"],format="%d/%m/%Y",errors="coerce")
+     df["Data"]=pd.to_datetime(df["Data"],format="%d/%m/%Y",errors="coerce").dt.date
      df["Número de Pessoas"]=pd.to_numeric(df["Número de Pessoas"],errors="coerce").fillna(0).astype(int)
      return df
 
