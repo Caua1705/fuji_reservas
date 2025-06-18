8eca9457c018cc4f4e1c65c3725bd9e318b08e01
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-18 11:01:09 -0300

    h

--- controller/reservas_controller.py
+++ controller/reservas_controller.py
@@ -2,6 +2,7 @@ import streamlit as st
 from services.reservas import processar_nova_reserva,processar_reservas_agrupadas
 from services.email import enviar_email_cliente
 from services.agrupar import agrupar_por_dia
+import pandas as pd
 
 def controlar_nova_reserva(df_reservas, data, dict_dados, aba):
     try:
@@ -14,5 +15,14 @@ def controlar_nova_reserva(df_reservas, data, dict_dados, aba):
         st.error(e)  # Exibe a mensagem da exceção (ex: excesso de reservas)
         st.stop() 
 
-def controlar_reservas_por_dia(df_reservas,filial,aba2,aba3):
-    processar_reservas_agrupadas(df_reservas,filial,aba2,aba3)
+def controlar_reservas_por_dia(df_reservas, filial, aba2, aba3):
+    if df_reservas is None:
+        st.error("df_reservas está vazio (None).")
+        return
+    if not isinstance(df_reservas, pd.DataFrame):
+        st.error(f"df_reservas tem tipo inválido: {type(df_reservas)}")
+        return
+    if df_reservas.empty:
+        st.warning("df_reservas está vazio.")
+        return
+    processar_reservas_agrupadas(df_reservas, filial, aba2, aba3)
