c4f54a6aac2044ee2a439bd2c72cda79cee9ef0e
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-18 14:58:25 -0300

    s

--- services/agrupar.py
+++ services/agrupar.py
@@ -1,4 +1,4 @@
 def agrupar_por_dia(df):
     df_agrupado = df.groupby("Data")["Número de Pessoas"].sum().reset_index()
-    df_agrupado["Data"] = df_agrupado["Data"].dt.date
+    df_agrupado["Data"]=df_agrupado["Data"].apply(lambda x:x.strftime("%d/%m/%Y"))
     return df_agrupado
