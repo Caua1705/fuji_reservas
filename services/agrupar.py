1ca471f764c6eb351a4242120b287fea2c5e93c4
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-19 15:00:13 -0300

    s

--- services/agrupar.py
+++ services/agrupar.py
@@ -3,5 +3,5 @@ from utils.dias_semana import DICIONARIO_DIAS
 def agrupar_por_dia(df,):
     df_agrupado = df.groupby("Data")["Número de Pessoas"].sum().reset_index()
     df_agrupado["Dia da semana"] = df_agrupado["Data"].apply(lambda x: DICIONARIO_DIAS[x.weekday()]) 
-    df_agrupado=[["Data","Dia da semana","Número de Pessoas"]]
+    df_agrupado=df_agrupado[["Data","Dia da semana","Número de Pessoas"]]
     return df_agrupado 
