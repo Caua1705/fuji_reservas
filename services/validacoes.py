013a168a1e5e6de2ebb9f59dd69fb4e6aac5b54d
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-20 08:24:14 -0300

    s

--- services/validacoes.py
+++ services/validacoes.py
@@ -1,5 +1,6 @@
+import streamlit as st
 def validar_reserva(df_filtrado,maximo_reservas):
-    
+    st.write(df_filtrado)
     soma_reservas=df_filtrado["Número de Pessoas"].sum()
     if soma_reservas>maximo_reservas:
         raise ValueError("O número de reservas é maior que o permitido.")
