78b8b39983a0a889a061a383bfad92d0315ee7a9
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-20 14:37:57 -0300

    x

--- pages/Gerenciar Reservas.py
+++ pages/Gerenciar Reservas.py
@@ -23,7 +23,6 @@ with col_filtros2:
     filial = st.selectbox("🏢 Filial", ["Aldeota", "Cambeba"])
 # Mostrar data selecionada
 # st.markdown(f"**📅 Data selecionada:** {data_selecionada.strftime('%d/%m/%Y')}")
-linha_divisoria()
 
 # Filtrar reservas por data
 reservas_dia = filtrar_df_reservas(df_reservas, data_selecionada)
