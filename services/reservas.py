64e6dc8afe3aec6bd1a17976b74c86c957601bba
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-20 11:36:24 -0300

    s

--- services/reservas.py
+++ services/reservas.py
@@ -34,8 +34,6 @@ def processar_reservas_agrupadas(df_reservas,filial,aba2,aba3):
 
 
 def exibir_resumo(df_reservas, ambiente):
-    st.write(df_reservas)
-    
     if df_reservas.empty:
         st.info(f"Nenhuma reserva no ambiente **{ambiente}** para esta data.")
         return
