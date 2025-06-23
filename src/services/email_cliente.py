cd7aebe19a3ee22d374e693698d1cbf9641eefaa
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-06-23 14:53:01 -0300

    j

--- src/services/email_cliente.py
+++ src/services/email_cliente.py
@@ -11,7 +11,7 @@ def enviar_email_cliente(email_origem, email_cliente, senha_app, nome_cliente, d
         msg["From"] = email_origem
         msg["To"] = email_cliente
 
-        pasta_template=Path(__file__).parens[2] / "templates" / "email"
+        pasta_template=Path(__file__).parents[2] / "templates" / "email"
         nome_template=pasta_template / "email_cliente"
         caminho_css=Path(__file__).parents[2] / "static" / "css" / "email_cliente.css"
         variaveis_template=variaveis_template_email_cliente(nome_cliente,data,hora,unidade,caminho_css)
