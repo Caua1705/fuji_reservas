cca82aaa4d8cdbc057e30c25282e2c35afdc1d13
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-07-01 09:58:23 -0300

    km

--- src/utils/formatadores.py
+++ src/utils/formatadores.py
@@ -5,8 +5,10 @@ def formatar_dados(df):
      df["Número de Pessoas"] = pd.to_numeric(df["Número de Pessoas"],errors="coerce").fillna(0).astype(int)
      return df
 
+
 def formatar_linhas_agrupadas(df_agrupado):
      return df_agrupado.applymap(str).values.tolist()
+
 
 def formatar_data_para_string(data):
     return data.strftime("%d/%m/%Y")
