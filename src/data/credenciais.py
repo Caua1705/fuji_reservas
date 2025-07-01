f317ca00e44bbf789df48968e5fd3a6ec41cf147
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-07-01 10:15:22 -0300

    op

--- src/data/credenciais.py
+++ src/data/credenciais.py
@@ -6,13 +6,6 @@ def buscar_credenciais(chave_secrets, caminho_json):
     try:
         return st.secrets[chave_secrets]
     except FileNotFoundError:
-<<<<<<< HEAD
-        import json
-        caminho_credenciais_google = Path(__file__).parents[2] / "credenciais_google.json"
-        with open(caminho_credenciais_google,"r") as credenciais:
-            credenciais_google=json.load(credenciais)
-    return credenciais_google
-=======
         with open(caminho_json,"r") as credenciais:
             return json.load(credenciais)
 
@@ -24,5 +17,4 @@ def get_credenciais_google():
 def get_credenciais_email():
     dados = buscar_credenciais("email",CAMINHOS["Credenciais_email"])
     return dados["origem"], dados["senha_app"]
->>>>>>> refatorando_codigos
 
