ebd16142d7b5c7bf5a88c57fe399c4f057f5b714
Cauã de Carvalho <cauaccarvalho10@gmail.com>
2025-07-03 14:08:52 -0300

    s

--- src/view/entrada_reserva.py
+++ src/view/entrada_reserva.py
@@ -5,24 +5,28 @@ from src.config.settings import HORARIOS_RESERVAS
 def obter_dados_reserva():
     st.markdown("## Nova Reserva")
 
-    with st.container():
-        st.markdown("#### Dados do Cliente e Reserva")
-        col1, col2 = st.columns(2)
+    st.markdown("#### Informações do Cliente")
+    col1, col2 = st.columns(2)
 
-        with col1:
-            nome = st.text_input("Nome completo *")
-            telefone = st.text_input("Telefone (somente números) *")
-            email = st.text_input("E-mail para confirmação *")
-            quantidade_pessoas = st.number_input("Número de pessoas *", min_value=1, value=1)
+    with col1:
+        nome = st.text_input("Nome completo *")
+        telefone = st.text_input("Telefone (somente números) *")
+        email = st.text_input("E-mail para confirmação *")
 
-        with col2:
-            data = st.date_input("Data da reserva *", value=datetime.today(), format="DD/MM/YYYY")
-            horario = st.selectbox("Horário da reserva *", HORARIOS_RESERVAS)
-            filial = st.selectbox("Unidade", ["Aldeota", "Cambeba"])
-            area = st.selectbox("Área do restaurante *", ["Interno", "Externo"])
+    with col2:
+        quantidade_pessoas = st.number_input("Número de pessoas *", min_value=1, value=1)
+        filial = st.selectbox("Unidade", ["Aldeota", "Cambeba"])
+        area = st.selectbox("Área do restaurante *", ["Interno", "Externo"])
+
+    st.markdown("#### Agendamento da Mesa")
+    col3, col4 = st.columns(2)
+    with col3:
+        data = st.date_input("Data *", value=datetime.today(), format="DD/MM/YYYY")
+    with col4:
+        horario = st.selectbox("Horário *", HORARIOS_RESERVAS)
 
     st.markdown("#### Observações (opcional)")
-    observacoes = st.text_area("", placeholder="Alguma solicitação especial para a reserva?")
+    observacoes = st.text_area("", placeholder="Deseja informar alguma preferência ou pedido especial?")
 
     return {
         "Data": data,
